3b194ef256bfff7f383fa5cbb65cf95c0e4b190f
Viacheslav Kobets <mertag_nu@ukr.net>
2023-06-21 10:32:32 +0300

    Added database MongoDB

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,9 @@
 const validateAddUpdate = require("./validateAddUpdate");
+const isValidId = require("./isValidId");
+const validateUpdateFavorite = require("./validateUpdateFavorite");
 
 module.exports = {
   validateAddUpdate,
+  isValidId,
+  validateUpdateFavorite,
 };
